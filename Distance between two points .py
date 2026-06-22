c93e6bbd623bfeedee8449b23bfe880dfc90feaa
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-06-22 22:35:56 +0530

    Implement distance calculation between two points

---
+++ Distance between two points .py
@@ -0,0 +1,7 @@
+import math
+x1=int(input("enter x1"))
+y1=int(input("enter y1"))
+x2=int(input("enter x2"))
+y2=int(input("enter y2"))
+distance=math.sqrt((x2-x1)**^2)+((y2-y1)**^2)
+print("distance is"distane)

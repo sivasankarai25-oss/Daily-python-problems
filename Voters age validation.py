9534bcf13fdf7cae81e81ef87bf571ea32006878
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-06-23 16:42:04 +0530

    Enhance voter age validation and input handling
    Added validation for birth year input and adjusted age calculation logic.

--- Voters age validation.py
+++ Voters age validation.py
@@ -1,14 +1,16 @@
 import datetime
 
-year_of_birth = int(input("In which year were you born? "))
+year_of_birth = int(input("Enter your birth year: "))
 
 current_year = datetime.datetime.now().year
-
-current_age = current_year - year_of_birth
-
-print("Your current age is", current_age)
-
-if current_age >= 18:
-    print("You are eligible to vote.")
+if year_of_birth > current_year or year_of_birth < 1900:
+    print("Invalid birth year!")
 else:
-    print("You are not eligible to vote.")
+    current_age = current_year - year_of_birth
+    print("Your current age is", current_age)
+
+    
+    if current_age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")

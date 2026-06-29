3b689c86e7bb80f5a7bf1d67292bfcf12ae025ff
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-06-29 23:14:34 +0530

    Implement student grade calculator

---
+++ Student grade calculator.py
@@ -0,0 +1,18 @@
+name=int(input("enter student name:,"))
+mark1=int(input("enter the mark1:,"))
+mark2=int(input("enter the mark2:,"))
+mark3=int(input("enter the mark3:,"))
+total=mark1+mark2+mark3
+average=total/3
+print("student:,"name)
+print("total:,"total)
+print("average:,"average
+if average>=90
+print("grade:",A)
+      elif average>=70
+      print("grade:",B)
+      elif average>=50
+      print("grade:",C)
+      elif average<50
+      print ("fail")
+      else print ("invaild grade")

8a1b547c950ec8c29dae506fa6ab32a121bb994a
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-06-30 23:19:09 +0530

    Implement dice rolling game with corrections
    Fix typos and correct the loop and function calls.

---
+++ Dice roll.py
@@ -0,0 +1,10 @@
+import random
+while true:
+  print("welcome to game")
+  print("press enter to roll the dice")
+  dice=radom.randit(1,6)
+  print("dice rooled",dice)
+  choice=input("want to roll again the fice(yes/no)")
+  if choice!= yes
+  print("thanks for playing")
+  break

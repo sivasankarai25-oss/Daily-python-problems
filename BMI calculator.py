17fbecbbbb6fe335e2b5474d583cb9315a6afad3
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-07-01 23:23:13 +0530

    Implement basic BMI calculator functionality

---
+++ BMI calculator.py
@@ -0,0 +1,6 @@
+print(welcome to the BMI calculator)
+BMI=weight/(height)**2
+weight=float(input(enter your weight,")
+height=float(input(enter your height in square,")
+result=print("your BMI is:",BMI)
+print("thankyou for using BMI calculator,come again")

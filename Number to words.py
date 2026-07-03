25f9d3bea96c6cc33e35ae91777f456127085e23
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-07-03 22:24:36 +0530

    Add number to words conversion for single digits

---
+++ Number to words.py
@@ -0,0 +1,24 @@
+print("Number to Words")
+number = int(input("Enter a single digit (0-9): "))
+if number == 0:
+    print("Zero")
+elif number == 1:
+    print("One")
+elif number == 2:
+    print("Two")
+elif number == 3:
+    print("Three")
+elif number == 4:
+    print("Four")
+elif number == 5:
+    print("Five")
+elif number == 6:
+    print("Six")
+elif number == 7:
+    print("Seven")
+elif number == 8:
+    print("Eight")
+elif number == 9:
+    print("Nine")
+else:
+    print("Invalid input!")

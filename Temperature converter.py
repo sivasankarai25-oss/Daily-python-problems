109de13d613e91f6019ec521e16196be298f5dbb
sivasankarai25-oss <sivasankar.ai25@stellamaryscoe.edu.in>
2026-07-08 23:01:20 +0530

    Add temperature converter functionality

---
+++ Temperature converter.py
@@ -0,0 +1,6 @@
+print("Welcome to Temperature Converter")
+celsius = float(input("Enter the temperature in Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print("Temperature in Celsius:", celsius)
+print("Temperature in Fahrenheit:", fahrenheit)
+print("Thank you for using Temperature Converter")
